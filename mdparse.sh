76ea7c8a210c3dad31ef1858f8953fd14fdd39ca
Phillip T Wu <cs15lsp22ajo@ieng6-201.ucsd.edu>
2022-05-08 15:49:37 -0700

    add bash script

--- mdparse.sh
+++ mdparse.sh
@@ -1,1 +1,1 @@
-java -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar:. MarkdownParse $1
+/software/CSE/oracle-java-17/jdk-17.0.1/bin/java -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar:. MarkdownParse $1
